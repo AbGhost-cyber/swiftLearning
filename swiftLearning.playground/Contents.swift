1ee570d55ceb4164c9f47b4c9878c82264395973
AbGhost-cyber <xplendo@gmail.com>
2022-12-08 10:10:47 +0800

    Initial Commit

---
+++ swiftLearning.playground/Contents.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+var greeting = "Hello, playground"
+ let quote = "He tapped \"Believe\" and walked"
+
+let movies = """
+A day in
+the life of an
+Apple engineer
+"""
+//print(movies.hasPrefix("A"))
+
+let score = 10
+let number = 120
+print(score.isMultiple(of: 10))
+
+let id = Int.random(in: 1...100)
+print(id)
+ 
