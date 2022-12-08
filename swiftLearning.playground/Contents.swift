f60b5793b39a0a6d9b939a7448450cf1d69dbf79
AbGhost-cyber <xplendo@gmail.com>
2022-12-08 10:27:16 +0800

    first commit

--- swiftLearning.playground/Contents.swift
+++ swiftLearning.playground/Contents.swift
@@ -17,3 +17,25 @@ print(score.isMultiple(of: 10))
 let id = Int.random(in: 1...100)
 print(id)
  
+let myScore = 3.0
+var goodDogs = true
+goodDogs.toggle()
+
+let name = "Taylor"
+let age = 23
+let message = "I am \(name) and I'm \(age) years old"
+
+var colors = ["Red", "Blue", "Orange"]
+let numbers = [1,2,3,4,5,6]
+colors.append("Black")
+
+let employee = [
+    "name": "Taylor",
+    "job":"Singer"
+]
+var numbersSet = Set([1,2,3,4,5])
+print(numbersSet)
+var nn:Set = [1,2,3,4,4,1,1]
+nn.insert(10)
+
+//enums
